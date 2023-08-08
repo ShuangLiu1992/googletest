95d0ced8082d656d75ca78768728bb516d80acf4
ShuangLiu <liushuang.work@gmail.com>
2023-08-08 12:04:07 +0100

    gmock

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 import conan.tools.files
-from conan.tools.cmake import CMake
+from conan.tools.cmake import CMake, CMakeToolchain
 import os
 
 
@@ -9,10 +9,15 @@ class GTESTConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    generators = "CMakeToolchain"
 
     def export_sources(self):
         conan.tools.files.copy(self, "*", self.recipe_folder, self.export_sources_folder)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        if self.settings.os == "Emscripten":
+            tc.variables["gtest_disable_pthreads"] = True
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
